d9af6e531c6319d2451b528707e454dcababd4d3
Janmejay2005 <86195670+Janmejay2005@users.noreply.github.com>
2024-12-25 14:55:26 +0530

    Update 1..c

--- 1..c
+++ 1..c
@@ -16,3 +16,12 @@ int main(){
     printf("Your Total Marks %.2f\n",s);
     printf("Your Total Percentage is %.2f",p);
 }
+
+#Algorithm
+Step 1 - Start
+Step 2 - Declare variables to store marks of 5 subjects, sum, and percentage.
+Step 3 - Input: Prompt the user to enter marks for each subject.
+Step 4 - Sum Calculation: Add the marks of all 5 subjects.
+Step 5 - Percentage Calculation: Calculate the percentage.
+Step 6 - Output: Print the sum and percentage.
+Atep 7 - End
