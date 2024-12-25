c7dd8528a4e182e1e3748d9511df6cca71ee89e3
Janmejay2005 <86195670+Janmejay2005@users.noreply.github.com>
2024-12-25 15:58:14 +0530

    Update 14.c

--- 14.c
+++ 14.c
@@ -13,3 +13,14 @@ int main() {
     printf("Sum of even numbers: %d\n", sumEven);
     printf("Sum of odd numbers: %d\n", sumOdd);
 }
+
+#Algorithm:
+Step 1 - Start
+Step 2 - Initialize: Declare variables for the sum of even numbers, sum of odd numbers, and the number N.
+Step 3 - Input: Prompt the user to enter a value for N.
+Step 4 - Sum Calculation:
+        Use a loop to iterate from 1 to N.
+        If the number is even, add it to the sum of even numbers.
+        If the number is odd, add it to the sum of odd numbers.
+Step 5 - Output: Display the sum of even and odd numbers.
+Step 6 - End
