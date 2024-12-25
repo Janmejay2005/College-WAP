0b2c4d61414143a36363c103df8e24bf4e3df0ea
Janmejay2005 <86195670+Janmejay2005@users.noreply.github.com>
2024-12-25 14:59:46 +0530

    Update 4.c

--- 4.c
+++ 4.c
@@ -6,3 +6,12 @@ int main() {
     f=(c*9/5)+32;
     printf("Temperature in Fahrenheit: %.2f\n", f);
 }
+
+#Algorithm 
+Step 1 - Start
+Step 2 - Initialize: Declare variables for temperature in Centigrade and Fahrenheit.
+Step 3 - Input: Prompt the user to enter the temperature in Centigrade.
+Step 4 - Conversion: Use the formula 𝐶/5=(𝐹−32)/9
+Step 5 - C/5=(F−32)/9 to convert Centigrade to Fahrenheit.
+Step 6 - Output: Display the temperature in Fahrenheit.
+Step 7 - End
