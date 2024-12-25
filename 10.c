89b3c6fcdf43ca531e8ccbdb8c6ee54addb60e5d
Janmejay2005 <86195670+Janmejay2005@users.noreply.github.com>
2024-12-25 15:37:05 +0530

    Update 10.c

--- 10.c
+++ 10.c
@@ -24,3 +24,17 @@ int main() {
     else 
         printf("Grade: D\n");
 }
+
+#Algorithm:
+Step 1 - Start
+Step 2 - Input: Declare variables for the marks of the five subjects and the total marks.
+Step 3 - Sum Calculation: Calculate the sum of the marks obtained in the five subjects.
+Step 4 - Percentage Calculation: Calculate the percentage using the formula:
+            Percentage= Total Marks Obtained×100/Total Marks
+Step 5 - Grade Determination: Based on the percentage, print the grade:
+        Between 90% and 100%: Print 'A'
+        Between 80% and 90%: Print 'B'
+        Between 60% and 80%: Print 'C'
+        Below 60%: Print 'D'
+Step 6 - Output: Print the percentage and the grade.
+Step 7 - End
