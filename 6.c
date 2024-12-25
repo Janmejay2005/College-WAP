3732e949adbfc8f6bac7161b3edada618d4270b0
Janmejay2005 <86195670+Janmejay2005@users.noreply.github.com>
2024-12-25 15:05:44 +0530

    Update 6.c

--- 6.c
+++ 6.c
@@ -10,3 +10,12 @@ int main(){
     else
         printf("Both Numbers are Not Equal");
 }
+
+#Algorithm:
+Step 1 - Start
+Step 2 - Initialize: Declare two variables to store the numbers.
+Step 3 - Input: Prompt the user to enter the first number and store it in the first variable.
+Step 4 - Input: Prompt the user to enter the second number and store it in the second variable.
+Step 5 - Comparison: Compare the two numbers.
+Step 6 - Output: If the numbers are equal, print that they are equal; otherwise, print that they are not equal.
+Step 7 - End
