80d1e092317d267f3a6ab8deb01d78ed0ac4bdbc
Janmejay2005 <86195670+Janmejay2005@users.noreply.github.com>
2024-12-26 09:40:33 +0530

    Update 2.c

--- 2.c
+++ 2.c
@@ -14,3 +14,14 @@ int main() {
     printf("Compound Interest: %.2lf\n", CI);
 }
 
+#Algorithm:
+Step 1 - Start
+Step 2 - Input: Prompt the user to enter Principal, Rate of Interest, and Time.
+Step 3 - Calculate Simple Interest using the formula:
+        SI=𝑃×𝑅×𝑇
+           _____
+            100
+​Step 4 - Calculate Compound Interest using the formula:
+        CI=P(1+ R/100)^t−P
+Step 5 - Output the Simple Interest and Compound Interest.
+Step 6 - End
