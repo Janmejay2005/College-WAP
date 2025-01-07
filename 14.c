183837cbd3bcfdbb29730578ee93b76ee22f52bc
Janmejay2005 <86195670+Janmejay2005@users.noreply.github.com>
2025-01-08 00:50:33 +0530

    Update 14.c

--- 14.c
+++ 14.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 int main() {
-    int N, sumEven = 0, sumOdd = 0;
+    int N, Even = 0, Odd = 0;
     printf("Enter a number N: ");
     scanf("%d", &N);
     for (int i = 1; i <= N; i++) {
         if (i % 2 == 0) { 
-            sumEven += i; 
+            Even += i; 
         } else {    
-            sumOdd += i; 
+            Odd += i; 
         }
     }
-    printf("Sum of even numbers: %d\n", sumEven);
-    printf("Sum of odd numbers: %d\n", sumOdd);
+    printf("Sum of even numbers: %d\n", Even);
+    printf("Sum of odd numbers: %d\n", Odd);
 }
 
 #Algorithm:
